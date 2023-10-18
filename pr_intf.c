81db6d4f6d626314969b446e3bb775967d2e35e6
marvcv <librarianmarvin@gmail.com>
2023-10-18 10:54:22 -0700

    print

--- pr_intf.c
+++ pr_intf.c
@@ -2,55 +2,45 @@
 
 int _printf(const char *format, ...)
 {
-	int cha_pr = 0;
-	va_list arglist;
+    if (format == NULL)
+        return -1;
 
-	if (format == NULL)
-		return (-1);
+    int cha_pr = 0;
+    va_list arglist;
+    va_start(arglist, format);
 
-	va_start(arglist, format);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c' || *format == 's')
+            {
+                if (*format == 'c')
+                    write(1, &va_arg(arglist, int), 1);
+                else
+                {
+                    char *str = va_arg(arglist, char*);
+                    int str_len = 0;
+                    while (str[str_len] != '\0')
+                        str_len++;
+                    write(1, str, str_len);
+                    cha_pr += str_len;
+                }
+            }
+            else if (*format == '%')
+                write(1, format, 1);
+            cha_pr += (*format == 'c' || *format == 's') ? 1 : 0;
+        }
+        else
+        {
+            write(1, format, 1);
+            cha_pr++;
+        }
+        format++;
+    }
 
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			cha_pr++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-				break;
+    va_end(arglist);
+    return cha_pr;
+}
 
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				cha_pr++;
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(arglist, int);
-
-				write(1, &c, 1);
-				cha_pr++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(arglist, char*);
-				int str_len = 0;
-
-				while (str[str_len] != '\0')
-					str_len++;
-
-				write(1, str, str_len);
-				cha_pr += str_len;
-			}
-		}
-
-		format++;
-	}
-
-	va_end(arglist);
-
-	return (cha_pr);
