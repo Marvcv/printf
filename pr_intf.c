46334b5fd0e02a9c6149e3ff935791c6ad730b6f
marvcv <librarianmarvin@gmail.com>
2023-10-18 23:12:54 -0700

    print

--- pr_intf.c
+++ pr_intf.c
@@ -13,51 +13,51 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-	
+
 	va_start(arglist, format);
-	
+
 	while (*format)
 	{
-                if (*format != '%')
-                {
-                        write(1, format, 1);
-                        cha_pr++;
-                }
-                else
-                {
-                        format++;
-                        if (*format == '\0')
-                                break;
+		if (*format != '%')
+		{
+			write(1, format, 1);
+				cha_pr++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
 
-                        if (*format == '%')
-                        {
-                                write(1, format, 1);
-                                cha_pr++;
-                        }
-                        else if (*format == 'c')
-                        {
-                                char c = va_arg(arglist, int);
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				cha_pr++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(arglist, int);
 
-                                write(1, &c, 1);
-                                cha_pr++;
-                        }
-                        else if (*format == 's')
-                        {
-                                char *str = va_arg(arglist, char*);
-                                int str_len = 0;
+				write(1, &c, 1);
+				cha_pr++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(arglist, char*);
+				int str_len = 0;
 
-                                while (str[str_len] != '\0')
-                                        str_len++;
+				while (str[str_len] != '\0')
+					str_len++;
 
-                                write(1, str, str_len);
-                                cha_pr += str_len;
-                        }
-                }
+				write(1, str, str_len);
+				cha_pr += str_len
+			}
+		}
 
-                format++;
-        }
+		format++;
+	}
 
-        va_end(arglist);
+	va_end(arglist);
 
-        return (cha_pr);
+	return (cha_pr);
 }
