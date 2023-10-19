8df893ee9020432ec83b21e6d75abf272b4dd2ca
marvcv <librarianmarvin@gmail.com>
2023-10-19 12:57:30 -0700

    dsd

--- main.h
+++ main.h
@@ -1,46 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_H
+#define HEADER_H
 
+/* C standard library header files */
 #include <stdarg.h>
-#include <string.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <limits.h>
 
-struct convert
-
-{
-
-	char *sym;
-
-	int (*f)(va_list);
-
-};
-
-typedef struct convert conver_t;
-
-
-
-
-
-int _putchar(char c);
-int _printf(const char *format, ...);
-void _vprintf(const char *format, va_list args);
-
-int parser(const char *format, conver_t funct_list[], va_list args);
-
-int p_char(va_list);
-
-int p_string(va_list args);
-
-int p_percent(va_list);
-
-int print_unsgined_number(unsigned int n);
-
-int print_number(va_list);
-
-int p_integer(va_list);
-
-
+/* function prototypes */
+int putchr(char z);
+int  my_printf(const char *format, ...);
 
 #endif
